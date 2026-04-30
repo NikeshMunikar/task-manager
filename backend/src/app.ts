f088d9e0d7dc6a8a471be4eee00716eab4b8df7e
Rakinum <bedrock062@gmail.com>
2026-04-30 07:09:05 +0545

    Added cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,12 +5,18 @@ import taskRoutes from "./routes/task.routes"
 import logger from "./middleware/logger.middleware"
 import errorMiddleware from "./middleware/error.middleware";
 import { authMiddleware, AuthRequest} from "./middleware/auth.middleware";
+import cors from "cors";
 
 const app = express();
 
+
 //Middleware
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(logger)
 app.use(express.json());
+
 
 
 
